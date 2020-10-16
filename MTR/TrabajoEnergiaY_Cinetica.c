4adcf986d53920e9966efbbec25360ef3e2fed03
AbrahamHRomo <conde_de_homero@hotmail.com>
2020-10-16 13:45:26 -0500

    Inicio Repo de Clases para facilitar el aprendisaje de los temas

---
+++ MTR/TrabajoEnergiaY_Cinetica.c
@@ -0,0 +1,32 @@
+/* *Que significa que una fuerza efectúe trabajo sobre un cuerpo,
+    y cómo calcular la cantidad de trabajo realizada.
+
+   *La definición de energía cinetica (energía de movimiento) de
+    un cuerpo, y lo que significa físicamente.
+
+   *Cómo el trabajo total efectuado sobre un cuerpo cambia la
+    energía cinética del cuerpo, y cómo utilizar este principio
+    para resolveror problemas de mecánica.
+
+   *Cómo usar la relación entre trabajo total y cambio de energía
+    cinética, cuando las fuerzas no son constantes y el cuerpo sigue una
+    trayectoria curva, o ambas situaciones.
+
+   *Cómo resolver problemas que implican ppotencia (tasa para
+    efectuar trabajo).
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+  float W_trabajo; //producto punto (escalar)
+  float F_magnitud_fuerza; // Newtons
+  float s_magnitud_x; //metros
+
+  // 1 joule = (1 newton)(1 metro) 0 bien   J = N * m
+  // 1 J = 0.7376 ft * lb    =>   ft * lb = 1.356 J
+
+  
+}
